728f2d946c22abe397754dce4296c69a7a1ef663
Sudiptoganguly <sudipto.ganguly@cbnits.com>
2019-12-10 14:20:22 +0530

    registration ang login model and seralizers

--- NewsApp/models.py
+++ NewsApp/models.py
@@ -1,3 +1,32 @@
-from django.db import models
+# from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class UserCredentials(models.Model):
+    email = models.EmailField(default="a@b.com")
+    username = models.TextField(max_length=20)
+    password = models.TextField(default="12345")
+    uni_id = models.TextField(default="abcd1234")
+
+    def __str__(self):
+        return self.email
+
+
+class UserDetail(models.Model):
+    fullname = models.CharField(max_length=30)
+    email = models.EmailField(default="a@abc.com")
+    password = models.TextField(default="12345")
+    userimage = models.ImageField(upload_to='Image/UserImage/')
+    usercreated = models.DateTimeField(auto_now_add=True)
+    lastlogin = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.email 
+   
+
+
+
+
+    
+    
